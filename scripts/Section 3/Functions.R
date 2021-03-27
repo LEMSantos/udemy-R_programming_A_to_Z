872007ad19d745175d5f9251cf99e18897ccbdad
LEMSantos <lucas_m-santos@hotmail.com>
2021-03-27 10:24:28 -0300

    Add - práticas da seção 3

---
+++ scripts/Section 3/Functions.R
@@ -0,0 +1,23 @@
+rnorm(n=5, sd=8)
+
+x <- c("a", "b", "c")
+
+c()
+A <- seq(from=10, to=20, along.with=x)
+rep(x, each=5)
+
+print()
+
+is.numeric()
+is.integer()
+is.double()
+is.character()
+
+typeof()
+
+A
+B <- sqrt(A)
+B
+paste()
+
+#?
